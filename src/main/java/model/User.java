d181f372abc373cf6e0e30e1b7bfabafbcb5dd56
feelsogoodryo <ryo.ohama0131@gmail.com>
2024-01-03 12:24:51 +0900

    mission10 finish

---
+++ src/main/java/model/User.java
@@ -0,0 +1,25 @@
+package model;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+	private String name; //uesr name
+	private String pass;//password
+
+	public User() {
+	}
+
+	public User(String name, String pass) {
+		this.name = name;
+		this.pass = pass;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+}
